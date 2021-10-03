912e0f4b0db69b7c470e25081c9f9a359dc735b9
CriScarFace <67079157+CriScarFace@users.noreply.github.com>
2021-10-03 13:07:38 -0300

    xD

--- Taller1/src/Taller1.java
+++ Taller1/src/Taller1.java
@@ -4,6 +4,7 @@ public abstract class Taller1 {
 	public static void main(String[] args) {
 		System.out.print("HOLA MUNDO1");
 		System.out.println("hola mundo 2");
+		System.out.println("Chupalo Brayan");
 
 	}
 
