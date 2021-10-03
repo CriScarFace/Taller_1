660099da753756067087b205969f29064bb5d150
pinhasaurio <brayan.maldonado@alumnos.ucn.cl>
2021-10-03 13:03:23 -0300

    holamundo 1 y 2

--- Taller1/src/Taller1.java
+++ Taller1/src/Taller1.java
@@ -2,7 +2,8 @@
 public abstract class Taller1 {
 
 	public static void main(String[] args) {
-		System.out.print("HOLA MUNDO");
+		System.out.print("HOLA MUNDO1");
+		System.out.println("hola mundo 2");
 
 	}
 
