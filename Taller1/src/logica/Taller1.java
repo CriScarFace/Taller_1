ca26b4c815ef20c1246e4a1a039d71460506bfea
pinhasaurio <brayan.maldonado@alumnos.ucn.cl>
2021-11-02 14:36:01 -0300

    comienzo app
    comenzamos a trabajar en la app

--- Taller1/src/logica/Taller1.java
+++ Taller1/src/logica/Taller1.java
@@ -3,14 +3,14 @@ package logica;
 public abstract class Taller1 {
 
 	public static void main(String[] args) {
-		System.out.print("HOLA MUNDO1");
-		System.out.println("hola mundo 2");
-		System.out.println("Chupalo Brayan");
+		Sistema s = new SistemaIMPL();
+		leerCuentas(s);
 
-		System.out.println("chupalo Cristian");
-		System.out.println("esperanza1");
+	}
 
-
+	private static void leerCuentas(Sistema s) {
+		boolean ingreso = true;// 02.11.2021 luego sigo
+		
 	}
 
 }
