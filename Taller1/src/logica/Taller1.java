0ad5ce2ee399c16466bc6d323594e4daef40b34c
CriScarFace <67079157+CriScarFace@users.noreply.github.com>
2021-11-07 01:13:50 -0300

    Update Taller1.java
    actualizado

--- Taller1/src/logica/Taller1.java
+++ Taller1/src/logica/Taller1.java
@@ -5,10 +5,6 @@ public abstract class Taller1 {
 	public static void main(String[] args) {
 		System.out.print("HOLA MUNDO1");
 		System.out.println("hola mundo 2");
-		System.out.println("Chupalo Brayan");
-
-		System.out.println("chupalo Cristian");
-		System.out.println("esperanza1");
 
 
 	}
